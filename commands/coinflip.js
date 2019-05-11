65e227b145c7d94c9e1308d8bd271e8cd8fd9780
Glitch (skytech) <none>
2019-05-11 17:04:14 +0000

    👗🎪 Updated with Glitch

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -1,10 +1,18 @@
-exports.run = function(client, msg, args, db) {
+exports.run = function(client, msg, args, db, RichEmbed) {
   var min = 0
   var max = 1
-  var num = Math.round(Math.random);
-  if (num == 0) var type = 'tails'
-  if (!type) var type = 'heads'
-  var img = `./assets/${type}.png`
+  var num = Math.round(Math.random());
+  if (num == 0) var type = 'Tails'
+  if (!type) var type = 'Heads'
+  var img = `assets/${type}.png`
+  var embed = new RichEmbed
+  embed.setTitle('Coinflip')
+  embed.setAuthor(msg.member.displayName, msg.author.avatarURL)
+  embed.setDescription(`${type}!`)
+  embed.setThumbnail(`https://skytech.glitch.me/file?dir=${img}`)
+  embed.setTimestamp();
+  embed.setColor(0x000000)
+  msg.channel.send(embed);
 }
 
 exports.info = {
