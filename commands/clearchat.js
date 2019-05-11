57689072c90e4467cce0447eab841cef53143b6a
Wyatt Lipscomb <35582368+GameFreako@users.noreply.github.com>
2019-05-11 13:43:01 -0400

    Create CMD !clearchat

---
+++ commands/clearchat.js
@@ -0,0 +1,9 @@
+exports.run = function(client, msg, args, db, RichEmbed) {
+  
+};
+
+exports.info = {
+  'description': 'Removes messages from the chat.',
+  'usage': '!clearchat <amount>',
+  'permission': 'MANAGE_MESSAGES'
+};
