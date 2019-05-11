263c8b811be1cf74796aab1351913227efd1b17c
Wyatt Lipscomb <35582368+GameFreako@users.noreply.github.com>
2019-05-11 13:27:16 -0400

    Fix

--- commands/verify.js
+++ commands/verify.js
@@ -1,11 +1,8 @@
 exports.run = function(client, msg, args, db, RichEmbed) {
-  var role = msg.guild.roles.find(ro => ro.name === "Verified")
-  if (!role) {
-    msg.channel.send("ERR404:NotFound")
-    msg.channel.send("[ERROR] An error has been detected. Please contact the developer to resolve this issue.")
-    msg.channel.send("404:NOTFOUND_ROLE-Verified")
-    return;
-  } else {
-    msg.channel.send('Account Verified.')
-  }
+  var rr = msg.guild.roles.find(ro => ro.name === "Unverified")
+  var ar = msg.guild.roles.find(ro => ro.name === "[Member]")
+  if (!ar) return;
+  if (!rr) return;
+  msg.member.addRole(ar);
+  msg.member.removeRole(rr);
 };
