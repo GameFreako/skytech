24a01121ba6d054be33679ae5eb745172cc025d0
Wyatt Lipscomb <35582368+GameFreako@users.noreply.github.com>
2019-05-11 13:46:41 -0400

    Code Clearchat CMD

--- commands/clearchat.js
+++ commands/clearchat.js
@@ -1,5 +1,17 @@
 exports.run = function(client, msg, args, db, RichEmbed) {
-  
+  msg.channel.fetchMessages({ 'limit': args[0] }).then(MD => {
+    var messages = MD.array();
+    var i;
+    var deleted = 0
+    for (i=0;i<messages.length;i++) {
+      var message = messages[i]
+      if (message.deleteable) {
+        message.delete()
+        deleted++
+      }
+    }
+    msg.channel.send(`Deleted ${deleted}/${args[0]} messages.`)
+  });
 };
 
 exports.info = {
